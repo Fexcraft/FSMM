d88cf2596224061ed417d242ed64505a735227ca
Ferdinand <fercalo96@yahoo.de>
2021-02-17 21:44:44 +0100

    Update GuiHandler.java

--- src/main/java/net/fexcraft/mod/fsmm/gui/GuiHandler.java
+++ src/main/java/net/fexcraft/mod/fsmm/gui/GuiHandler.java
@@ -7,6 +7,12 @@ import net.minecraftforge.fml.common.network.IGuiHandler;
 public class GuiHandler implements IGuiHandler {
 
 	public static final int ATM_MAIN = 0;
+	public static final int BANK_INFO = 10;
+	public static final int BANK_SELECT = 11;
+	public static final int ACCOUNT_SELECT = 20;
+	public static final int ACCOUNT_WITHDRAW = 21;
+	public static final int ACCOUNT_DEPOSIT= 22;
+	public static final int ACCOUNT_TRANSFER = 23;
 	
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
