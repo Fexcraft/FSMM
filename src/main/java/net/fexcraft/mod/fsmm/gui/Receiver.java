edc7b1d88e2fa7d28c6ef90390bcb4de674e9677
Ferdinand <fercalo96@yahoo.de>
2021-02-25 16:47:08 +0100

    Update Receiver.java

--- src/main/java/net/fexcraft/mod/fsmm/gui/Receiver.java
+++ src/main/java/net/fexcraft/mod/fsmm/gui/Receiver.java
@@ -5,7 +5,6 @@ import net.fexcraft.lib.mc.network.packet.PacketNBTTagCompound;
 import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.fsmm.util.Config;
 import net.fexcraft.mod.fsmm.util.Config.SyncableConfig;
-import net.minecraft.nbt.NBTTagList;
 
 public class Receiver implements IPacketListener<PacketNBTTagCompound> {
 
@@ -19,34 +18,11 @@ public class Receiver implements IPacketListener<PacketNBTTagCompound> {
 		Print.debug(pkt.nbt);
 		if(pkt.nbt.hasKey("payload")){
 			switch(pkt.nbt.getString("payload")){
-				case "main_data":{
-					AutomatedTellerMashineGui.INSTANCE.openPerspective("main", pkt.nbt);
-					break;
-				}
-				case "show_balance":{
-					AutomatedTellerMashineGui.INSTANCE.openPerspective("show_balance", pkt.nbt);
-					break;
-				}
-				case "deposit_result":
-				case "withdraw_result":{
-					AutomatedTellerMashineGui.INSTANCE.openPerspective(pkt.nbt.getString("payload"), pkt.nbt);
-					break;
-				}
-				case "account_types":{
-					AutomatedTellerMashineGui.INSTANCE.catlist = (NBTTagList)pkt.nbt.getTag("types");
-					break;
-				}
-				case "accounts_of_type":{
-					AutomatedTellerMashineGui.INSTANCE.idlist = (NBTTagList)pkt.nbt.getTag("accounts");
-					break;
-				}
-				case "transfer_result":{
-					AutomatedTellerMashineGui.INSTANCE.openPerspective(pkt.nbt.getString("payload"), pkt.nbt);
-					break;
-				}
 				case "config_sync":{
-					Config.REMOTE = SyncableConfig.fromNBT(pkt.nbt); Config.REMOTE.apply();
-					Print.log("[FSMM] Received Config Sync Packet from Server."); break;
+					Config.REMOTE = SyncableConfig.fromNBT(pkt.nbt);
+					Config.REMOTE.apply();
+					Print.log("[FSMM] Received Config Sync Packet from Server.");
+					break;
 				}
 			}
 		}
