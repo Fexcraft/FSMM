6addef93a25e03cef7d85da4745ba00ab2697679
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-12 13:06:51 +0200

    Update CapabilityEvents.java

--- src/main/java/net/fexcraft/mod/fsmm/events/CapabilityEvents.java
+++ src/main/java/net/fexcraft/mod/fsmm/events/CapabilityEvents.java
@@ -2,7 +2,7 @@ package net.fexcraft.mod.fsmm.events;
 
 import net.fexcraft.mod.fsmm.data.FSMMCapabilities;
 import net.fexcraft.mod.fsmm.data.PlayerCapability;
-import net.fexcraft.mod.fsmm.impl.PlayerCapabilityUtil;
+import net.fexcraft.mod.fsmm.data.cap.PlayerCapSerializer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,7 +14,7 @@ public class CapabilityEvents {
     @SubscribeEvent
     public static void onAttachEntityCapabilities(AttachCapabilitiesEvent<net.minecraft.entity.Entity> event){
     	if(FSMMCapabilities.PLAYER != null && event.getObject() instanceof EntityPlayer){
-    		event.addCapability(PlayerCapability.REGISTRY_NAME, new PlayerCapabilityUtil((EntityPlayer)event.getObject()));
+    		event.addCapability(PlayerCapability.REGISTRY_NAME, new PlayerCapSerializer((EntityPlayer)event.getObject()));
     	}
     }
 
