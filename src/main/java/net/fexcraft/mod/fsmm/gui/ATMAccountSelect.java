7b62ca635253e3649b61b44fd35556f88a163b88
Ferdinand Calo <fexcalo@gmail.com>
2024-01-24 22:46:57 +0100

    Update ATMAccountSelect.java

--- src/main/java/net/fexcraft/mod/fsmm/gui/ATMAccountSelect.java
+++ src/main/java/net/fexcraft/mod/fsmm/gui/ATMAccountSelect.java
@@ -27,11 +27,12 @@ public class ATMAccountSelect extends GenericGui<ATMContainer> {
 	private BasicText acc0, acc1;
 	private BasicText[] accs = new BasicText[8];
 	private BasicButton[] acc = new BasicButton[4];
-	private int scroll, mode;
+	private int scroll;
+	private boolean mode;
 
-	public ATMAccountSelect(EntityPlayer player, int mode){
-		super(texture, new ATMContainer(player), player);
-		this.mode = mode;
+	public ATMAccountSelect(EntityPlayer player, boolean bool, int[] pos){
+		super(texture, new ATMContainer(player, pos), player);
+		this.mode = bool;
 		this.xSize = 256;
 		this.ySize = 152;
 	}
@@ -53,8 +54,8 @@ public class ATMAccountSelect extends GenericGui<ATMContainer> {
 		this.buttons.put("dw", dw = new BasicButton("dw", guiLeft + 237, guiTop + 144, 237, 144, 7, 7, true));
 		fields.put("type", new TextField(0, fontRenderer, guiLeft + 6, guiTop + 32, 235, 8).setEnableBackground(false));
 		fields.put("id", new TextField(1, fontRenderer, guiLeft + 6, guiTop + 42, 226, 8).setEnableBackground(false));
-		this.container.sync("account", mode == 0 ? "account_list" : "");
-		if(mode == 1){
+		this.container.sync("account", mode ? "account_list" : "");
+		if(!mode){
 			accs[2].string = "Please enter a type and";
 			accs[3].string = "name/id then press search!";
 		}
@@ -98,7 +99,7 @@ public class ATMAccountSelect extends GenericGui<ATMContainer> {
 			for(int i = 0; i < 4; i++){
 				if(!acc[i].hovered || i + scroll >= accounts.size()) continue;
 				tooltip.add(Formatter.format("&7Click to select this account."));
-				if(mode == 0){
+				if(mode){
 					AccountPermission perm = accounts.get(i + scroll);
 					tooltip.add(Formatter.format("&9Permissions:"));
 					tooltip.add(Formatter.format("&7Withdraw: " + (perm.withdraw ? "&atrue" : "&cfalse")));
@@ -112,12 +113,12 @@ public class ATMAccountSelect extends GenericGui<ATMContainer> {
 			tooltip.add(Formatter.format("&7Type of &9Account &7to be &6searched&7."));
 			tooltip.add(Formatter.format("&7(enter the full type name)."));
 			tooltip.add(Formatter.format("&9&oFor Player accounts type in \"player\"."));
-			if(mode == 1) tooltip.add(Formatter.format("&c&o*required"));
+			if(!mode) tooltip.add(Formatter.format("&c&o*required"));
 		}
 		if(uid.hovered){
 			tooltip.add(Formatter.format("&7ID/Name of &9Account &7to be &6searched&7."));
 			tooltip.add(Formatter.format(Config.PARTIAL_ACCOUNT_NAME_SEARCH ? "&7(you can just write bits of the name/id)" : "&7(enter the full account name/id)"));
-			if(mode == 1) tooltip.add(Formatter.format("&c&o*required"));
+			if(!mode) tooltip.add(Formatter.format("&c&o*required"));
 		}
 		if(search.hovered) tooltip.add(Formatter.format("&7Search/Filter"));
 		if(up.hovered) tooltip.add(Formatter.format("&7Scroll Up"));
@@ -135,7 +136,7 @@ public class ATMAccountSelect extends GenericGui<ATMContainer> {
 			compound.setString("cargo", "account_select");
 			compound.setString("type", perm.getAccount().getType());
 			compound.setString("id", perm.getAccount().getId());
-			compound.setInteger("mode", mode);
+			compound.setBoolean("mode", mode);
 			container.send(Side.SERVER, compound);
 			return true;
 		}
@@ -161,24 +162,24 @@ public class ATMAccountSelect extends GenericGui<ATMContainer> {
 		if(accounts != null) accounts.clear();
 		String type = fields.get("type").getText(), id = fields.get("id").getText();
 		boolean notype = type.trim().length() == 0, noid = id.trim().length() == 0;
-		if(mode == 1 && notype){
+		if(!mode && notype){
 			Print.chat(player, "&cYou need to enter the searched account type.");
 			return;
 		}
-		if(mode == 1 && (noid || id.length() < Config.MIN_SEARCH_CHARS)){
+		if(!mode && (noid || id.length() < Config.MIN_SEARCH_CHARS)){
 			Print.chat(player, "&cYou need to enter at least " + Config.MIN_SEARCH_CHARS + " characters of the searched id.");
 			return;
 		}
 		if(notype && noid){
-			if(mode == 0) accounts.addAll(container.accounts);
+			if(mode) accounts.addAll(container.accounts);
 			return;
 		}
-		if(mode == 0){
+		if(mode){
 			accounts = container.accounts.stream().filter(acc -> {
 				return (notype || acc.getAccount().getType().equals(type)) && (noid || acc.getAccount().getId().contains(id) || acc.getAccount().getName().toLowerCase().contains(id));
 			}).collect(Collectors.toList());
 		}
-		if(mode == 1){
+		if(!mode){
 			NBTTagCompound compound = new NBTTagCompound();
 			compound.setString("cargo", "account_search");
 			compound.setString("type", type);
@@ -198,8 +199,8 @@ public class ATMAccountSelect extends GenericGui<ATMContainer> {
 	@Override
     public void keyTyped(char typedChar, int keyCode) throws IOException{
         if(keyCode == 1){
-    		if(mode == 0) openGui(GuiHandler.ATM_MAIN, new int[]{ 0, 0, 0 }, LISTENERID);
-    		if(mode == 1) openGui(GuiHandler.ACCOUNT_TRANSFER, new int[]{ 0, 0, 0 }, LISTENERID);
+    		if(mode) openGui(GuiHandler.ATM_MAIN, container.pos, LISTENERID);
+    		else openGui(GuiHandler.ACCOUNT_TRANSFER, container.pos, LISTENERID);
             return;
         }
         else super.keyTyped(typedChar, keyCode);
