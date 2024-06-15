18fb05d2538e5c677d84b5cad257232331dad28e
Ferdinand Calo <fexcalo@gmail.com>
2024-06-15 18:04:40 +0200

    Update Removable.java

--- common/java/net/fexcraft/mod/fsmm/data/Removable.java
+++ common/java/net/fexcraft/mod/fsmm/data/Removable.java
@@ -1,11 +1,9 @@
 package net.fexcraft.mod.fsmm.data;
 
-import com.google.gson.JsonObject;
-
 import net.fexcraft.lib.common.math.Time;
 
 /**
- * Internal Usage Class, do not bother with.
+ * Internal Usage Class.
  * 
  * @author Ferdinand Calo' (FEX___96)
  */
@@ -14,16 +12,14 @@ public abstract class Removable {
 	private boolean temporary;
 	private long last_access;
 	
-	/** Time of when this Account/Bank was last accessed, used for removing temporary loaded account/bankss. */
+	/** Time of when this Account/Bank was last accessed, used for removing temporary loaded account/banks. */
 	public long lastAccessed(){
 		return temporary ? last_access : -1;
 	}
-	
-	/** Self explaining. */
+
 	public long updateLastAccess(){
 		return last_access = temporary ? Time.getDate() : -1;
 	}
-	
 	
 	/** Set this instance as "temporary loaded", as such, to be removed next check for inactive accounts/banks.*/
 	public <T extends Removable> T setTemporary(boolean value){
