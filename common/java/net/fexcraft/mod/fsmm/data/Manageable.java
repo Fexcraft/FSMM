5800b08034eddb1a7cc82b57b6f87888c29743c9
Ferdinand Calo <fexcalo@gmail.com>
2024-06-16 19:22:35 +0200

    Update Manageable.java

--- common/java/net/fexcraft/mod/fsmm/data/Manageable.java
+++ common/java/net/fexcraft/mod/fsmm/data/Manageable.java
@@ -1,17 +1,17 @@
 package net.fexcraft.mod.fsmm.data;
 
 import net.fexcraft.app.json.JsonMap;
-import net.minecraft.command.ICommandSender;
+import net.fexcraft.mod.uni.world.MessageSender;
 
 /**
- * Internal Usage Class, do not bother with.
+ * Internal Usage Class.
  * 
  * @author Ferdinand Calo' (FEX___96)
  */
 
 public interface Manageable {
 	
-	public void modifyBalance(Action action, long amount, ICommandSender log);
+	public void modifyBalance(Action action, long amount, MessageSender log);
 	
 	public static enum Action {
 		ADD, SUB, SET;
