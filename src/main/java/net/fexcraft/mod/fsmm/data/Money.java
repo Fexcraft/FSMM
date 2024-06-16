df63db5760b9cec91d730dad810f53217ae47b1a
Ferdinand Calo <fexcalo@gmail.com>
2024-06-16 20:21:42 +0200

    Update Money.java

--- src/main/java/net/fexcraft/mod/fsmm/data/Money.java
+++ src/main/java/net/fexcraft/mod/fsmm/data/Money.java
@@ -5,6 +5,9 @@ import net.fexcraft.lib.common.math.Time;
 import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.lib.mc.utils.Static;
 import net.fexcraft.mod.fsmm.FSMM;
+import net.fexcraft.mod.uni.IDL;
+import net.fexcraft.mod.uni.IDLManager;
+import net.fexcraft.mod.uni.impl.IDLM;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.JsonToNBT;
 import net.minecraft.nbt.NBTException;
@@ -12,17 +15,20 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
-public class Money implements IForgeRegistryEntry<Money> {
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class Money {
 
-	private ResourceLocation regname;
+	private IDL regname;
 	private ItemStack stack;
 	private long worth;
 
 	public Money(JsonMap map, boolean internal){
-		regname = new ResourceLocation((internal ? FSMM.MODID + ":" : "") + map.getString("id", "invalid_" + map.toString() + "_" + Time.getDate()));
+		regname = IDLManager.getIDLCached((internal ? FSMM.MODID + ":" : "") + map.getString("id", "invalid_" + map.toString() + "_" + Time.getDate()));
 		worth = map.getLong("worth", -1);
 		int meta = map.getInteger("meta", -1);
-		if(meta >= 0 && !internal) regname = new ResourceLocation(regname.toString() + "_" + meta);
+		if(meta >= 0 && !internal) regname = IDLManager.getIDLCached(regname.colon() + "_" + meta);
 		if(!internal){
 			stackload(null, map, internal);
 		}
@@ -52,20 +58,8 @@ public class Money implements IForgeRegistryEntry<Money> {
 		if(compound != null){ stack.setTagCompound(compound); }
 	}
 
-	@Override
-	public Money setRegistryName(ResourceLocation name){
-		regname = name;
-		return this;
-	}
-
-	@Override
-	public ResourceLocation getRegistryName(){
+	public IDL getID(){
 		return regname;
-	}
-
-	@Override
-	public Class<Money> getRegistryType(){
-		return Money.class;
 	}
 
 	@Override
