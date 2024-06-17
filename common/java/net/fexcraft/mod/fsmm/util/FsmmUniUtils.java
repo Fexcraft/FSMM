1b24f3cf64539557974e0c4c52876e9eb98e3cc6
Ferdinand Calo <fexcalo@gmail.com>
2024-06-17 22:35:08 +0200

    Create FsmmUniUtils.java

---
+++ common/java/net/fexcraft/mod/fsmm/util/FsmmUniUtils.java
@@ -0,0 +1,14 @@
+package net.fexcraft.mod.fsmm.util;
+
+import net.fexcraft.mod.fsmm.data.Account;
+
+import java.util.function.Function;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class FsmmUniUtils {
+
+	public static Function<Object, Account> GET_PLAYER_ACCOUNT = null;
+
+}
