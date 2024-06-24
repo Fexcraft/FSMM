6b023c108683bfb6f03a31899f86bf3c66a7ddc3
Ferdinand Calo <fexcalo@gmail.com>
2024-06-24 15:07:10 +0200

    Create PlayerAccData.java

---
+++ common/java/net/fexcraft/mod/fsmm/data/PlayerAccData.java
@@ -0,0 +1,107 @@
+package net.fexcraft.mod.fsmm.data;
+
+import net.fexcraft.mod.fsmm.util.DataManager;
+import net.fexcraft.mod.fsmm.util.ItemManager;
+import net.fexcraft.mod.uni.UniPlayer;
+import net.fexcraft.mod.uni.tag.TagCW;
+
+import java.util.UUID;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class PlayerAccData implements UniPlayer.AppData {
+
+	private UniPlayer player;
+	private Account account;
+	private AccountPermission atmacc;
+	private Account selected;
+	private Bank atmbank;
+
+	public Account getAccount(){
+		if(account == null){
+			account = DataManager.getAccount("player:" + player.entity.getUUID().toString(), false, true);
+			account.setName(player.entity.getName());
+		}
+		return account;
+	}
+
+	public Bank getBank(){
+		return getAccount().getBank();
+	}
+
+	/** Returns the worth of all (as money defined) Items in Inventory. **/
+	public long getMoneyInInventory(){
+		return ItemManager.countInInventory(player.entity.local());
+	}
+
+	/** Tries to subtract the defined amount from Inventory, <s>returns the amount which couldn't be subtracted. **/
+	public long subMoneyFromInventory(long expected_amount){
+		return ItemManager.removeFromInventory(player.entity.local(), expected_amount);
+	}
+
+	/** Tries to add the defined amount to Inventory, <s>returns the amount which couldn't be added.</s> **/
+	public long addMoneyToInventory(long expected_amount){
+		return ItemManager.addToInventory(player.entity.local(), expected_amount);
+	}
+
+	/** Tries to add the defined amount to Inventory, <s>returns the amount which couldn't be processed.</s> **/
+	public long setMoneyInInventory(long expected_amount){
+		return ItemManager.setInInventory(player.entity.local(), expected_amount);
+	}
+
+	/** Returns the currently/last selected Account in the ATM. */
+	public AccountPermission getSelectedAccount(){
+		return atmacc;
+	}
+
+	/** Sets the currently/last selected Account in the ATM. */
+	public void setSelectedAccount(AccountPermission perm){
+		atmacc = perm;
+	}
+
+	/** Returns the currently selected receiver Account in the ATM. */
+	public Account getSelectedReceiver(){
+		return selected;
+	}
+
+	/** Sets the currently selected receiver Account in the ATM. */
+	public void setSelectedReceiver(Account account){
+		selected = account;
+	}
+
+	/** Returns the currently looked at Bank in the ATM. */
+	public Bank getSelectedBankInATM(){
+		return atmbank;
+	}
+
+	/** Sets the currently looked at Bank in the ATM. */
+	public void setSelectedBankInATM(Bank bank){
+		atmbank = bank;
+	}
+
+	public Object getPlayer(){
+		return player.entity.direct();
+	}
+
+	@Override
+	public void save(UniPlayer player, TagCW com){
+		//
+	}
+
+	@Override
+	public void load(UniPlayer player, TagCW com){
+		//
+	}
+
+	@Override
+	public void set(UniPlayer player){
+		this.player = player;
+	}
+
+	@Override
+	public String id(){
+		return "fsmm";
+	}
+
+}
