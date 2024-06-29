03462952bc6dd8aa2bd5addb69e21458a30f5887
Ferdinand Calo <fexcalo@gmail.com>
2024-06-29 08:47:22 +0200

    Update FsmmUIKeys.java

--- common/java/net/fexcraft/mod/fsmm/util/FsmmUIKeys.java
+++ common/java/net/fexcraft/mod/fsmm/util/FsmmUIKeys.java
@@ -11,15 +11,25 @@ import java.util.function.BiFunction;
  */
 public class FsmmUIKeys {
 
-	public static final UIKey UI_ATM_MAIN = new UIKey(1, "fsmm:atm_main");
-	public static final UIKey UI_ATM_BANK_INFO = new UIKey(10, "fsmm:atm_bank_info");
-	public static final UIKey UI_ATM_BANK_SELECT = new UIKey(11, "fsmm:atm_bank_select");
-	public static final UIKey UI_ATM_ACC_SELECT = new UIKey(20, "fsmm:atm_account_select");
-	public static final UIKey UI_ATM_ACC_RECEIVER = new UIKey(21, "fsmm:atm_account_receiver");
-	public static final UIKey UI_ATM_ACC_WITHDRAW = new UIKey(22, "fsmm:atm_account_withdraw");
-	public static final UIKey UI_ATM_ACC_DEPOSIT = new UIKey(23, "fsmm:atm_account_deposit");
-	public static final UIKey UI_ATM_ACC_TRANSFER = new UIKey(24, "fsmm:atm_account_transfer");
-	public static final UIKey UI_ATM_TRANSFERS = new UIKey(25, "fsmm:atm_transfers");
+	public static final int ID12_ATM_MAIN = 1;
+	public static final int ID12_ATM_BANK_INFO = 10;
+	public static final int ID12_ATM_BANK_SELECT = 11;
+	public static final int ID12_ATM_ACC_SELECT = 20;
+	public static final int ID12_ATM_ACC_RECEIVER = 21;
+	public static final int ID12_ATM_ACC_WITHDRAW = 22;
+	public static final int ID12_ATM_ACC_DEPOSIT = 23;
+	public static final int ID12_ATM_ACC_TRANSFER = 24;
+	public static final int ID12_ATM_TRANSFERS = 30;
+
+	public static final UIKey UI_ATM_MAIN = new UIKey(ID12_ATM_MAIN, "fsmm:atm_main");
+	public static final UIKey UI_ATM_BANK_INFO = new UIKey(ID12_ATM_BANK_INFO, "fsmm:atm_bank_info");
+	public static final UIKey UI_ATM_BANK_SELECT = new UIKey(ID12_ATM_BANK_SELECT, "fsmm:atm_bank_select");
+	public static final UIKey UI_ATM_ACC_SELECT = new UIKey(ID12_ATM_ACC_SELECT, "fsmm:atm_account_select");
+	public static final UIKey UI_ATM_ACC_RECEIVER = new UIKey(ID12_ATM_ACC_RECEIVER, "fsmm:atm_account_receiver");
+	public static final UIKey UI_ATM_ACC_WITHDRAW = new UIKey(ID12_ATM_ACC_WITHDRAW, "fsmm:atm_account_withdraw");
+	public static final UIKey UI_ATM_ACC_DEPOSIT = new UIKey(ID12_ATM_ACC_DEPOSIT, "fsmm:atm_account_deposit");
+	public static final UIKey UI_ATM_ACC_TRANSFER = new UIKey(ID12_ATM_ACC_TRANSFER, "fsmm:atm_account_transfer");
+	public static final UIKey UI_ATM_TRANSFERS = new UIKey(ID12_ATM_TRANSFERS, "fsmm:atm_transfers");
 
 	public static BiFunction<UniEntity, V3I, Boolean> IS_ATM = null;
 
