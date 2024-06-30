99c993e601440324e6f6f040ca481de20c9e72c6
Ferdinand Calo <fexcalo@gmail.com>
2024-06-30 23:33:04 +0200

    Update GuiHandler.java

--- src/main/java/net/fexcraft/mod/fsmm/gui/GuiHandler.java
+++ src/main/java/net/fexcraft/mod/fsmm/gui/GuiHandler.java
@@ -10,6 +10,9 @@ import net.minecraftforge.fml.common.network.IGuiHandler;
 
 import static net.fexcraft.mod.fsmm.util.FsmmUIKeys.*;
 
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
 public class GuiHandler implements IGuiHandler {
 	
 	@Override
@@ -20,35 +23,17 @@ public class GuiHandler implements IGuiHandler {
 				return null;
 			}
 		}
-		//return new ATMContainer(player, new int[]{ x, y, z });
 		return UIUtils.getServer("fsmm", ID, player, x, y, z);
 	}
 
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z){
-		switch(ID){
-			case ID12_ATM_MAIN:
-				return UIUtils.getClient("fsmm", ID, player, x, y, z);
-			case ID12_ATM_BANK_INFO:
-				return new ATMBankInfo(player, new int[]{ x, y, z });
-			case ID12_ATM_BANK_SELECT:
-				return new ATMBankSelect(player, new int[]{ x, y, z });
-			case ID12_ATM_TRANSFERS:
-				return new ATMViewTransfers(player, new int[]{ x, y, z });
-			case ID12_ATM_ACC_SELECT:
-			case ID12_ATM_ACC_RECEIVER:
-				return new ATMAccountSelect(player, ID == ID12_ATM_ACC_SELECT, new int[]{ x, y, z });
-			case ID12_ATM_ACC_WITHDRAW:
-			case ID12_ATM_ACC_DEPOSIT:
-				if(player.world.getBlockState(new BlockPos(x, y, z)).getBlock() instanceof ATM == false){
-					Print.chat(player, "Action not available via Mobile Banking.");
-					return null;
-				}
-				return new ATMAccountSelf(player, ID == ID12_ATM_ACC_DEPOSIT, new int[]{ x, y, z });
-			case ID12_ATM_ACC_TRANSFER:
-				return new ATMAccountTransfer(player, new int[]{ x, y, z });
-			default:
+		if(ID == ID12_ATM_ACC_DEPOSIT || ID == ID12_ATM_ACC_WITHDRAW){
+			if(player.world.getBlockState(new BlockPos(x, y, z)).getBlock() instanceof ATM == false){
+				Print.chat(player, "Action not available via Mobile Banking.");
 				return null;
+			}
 		}
+		return UIUtils.getClient("fsmm", ID, player, x, y, z);
     }
 }
