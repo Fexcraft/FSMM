e5678a85f89d45414ed2d6a2164863fd6859fbc3
Ferdinand Calo <fexcalo@gmail.com>
2025-04-23 21:25:08 +0200

    Fabric 1.21

---
+++ fabric21/src/main/java/net/fexcraft/mod/fsmm/FSMM.java
@@ -0,0 +1,18 @@
+package net.fexcraft.mod.fsmm;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FSMM implements ModInitializer {
+
+	public static final String MODID = "fsmm";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize(){
+		//
+	}
+
+}
