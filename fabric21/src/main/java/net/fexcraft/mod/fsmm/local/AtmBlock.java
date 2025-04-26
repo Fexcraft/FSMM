196f4764fdcc3d123bf3efdfa1db7637929bfd0f
Ferdinand Calo <fexcalo@gmail.com>
2025-04-26 23:27:47 +0200

    Create AtmBlock.java

---
+++ fabric21/src/main/java/net/fexcraft/mod/fsmm/local/AtmBlock.java
@@ -0,0 +1,47 @@
+package net.fexcraft.mod.fsmm.local;
+
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.fsmm.util.FsmmUIKeys;
+import net.fexcraft.mod.uni.UniEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.BlockHitResult;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class AtmBlock extends Block {
+
+	//public static final DirectionProperty FACING = DirectionProperty.create("facing", Direction.Plane.HORIZONTAL);
+
+	public AtmBlock(Properties prop){
+		super(prop.noOcclusion().mapColor(MapColor.STONE));
+	}
+
+	@Override
+    public InteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult res){
+		if(!level.isClientSide && hand != InteractionHand.OFF_HAND){
+			UniEntity.get(player).entity.openUI(FsmmUIKeys.UI_ATM_MAIN, new V3I(pos.getX(), pos.getY(), pos.getZ()));
+        }
+		return InteractionResult.SUCCESS;
+    }
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> sd){
+		//sd.add(FACING);
+	}
+
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		return defaultBlockState();//.setValue(FACING, context.getPlayer().getDirection().getOpposite());
+	}
+
+}
