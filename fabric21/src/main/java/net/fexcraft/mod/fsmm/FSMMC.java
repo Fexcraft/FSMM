8ff0589069fe92e89c9d58a6d892a9cd94643d45
Ferdinand Calo <fexcalo@gmail.com>
2025-04-26 23:54:01 +0200

    Update FSMMC.java

--- fabric21/src/main/java/net/fexcraft/mod/fsmm/FSMMC.java
+++ fabric21/src/main/java/net/fexcraft/mod/fsmm/FSMMC.java
@@ -2,7 +2,17 @@ package net.fexcraft.mod.fsmm;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fexcraft.mod.fsmm.local.AtmBlock;
+import net.fexcraft.mod.fsmm.local.MobileAtm;
+import net.fexcraft.mod.fsmm.local.MoneyItem;
 import net.fexcraft.mod.fsmm.util.Config;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 
 import static net.fexcraft.mod.fsmm.local.FsmmCmd.getFormatted;
 
@@ -10,6 +20,8 @@ import static net.fexcraft.mod.fsmm.local.FsmmCmd.getFormatted;
  * @author Ferdinand Calo' (FEX___96)
  */
 public class FSMMC implements ClientModInitializer {
+
+	public static CreativeModeTab TAB;
 
 	@Override
 	public void onInitializeClient(){
@@ -24,6 +36,16 @@ public class FSMMC implements ClientModInitializer {
 			}
 			lines.add(getFormatted(str));
 		});
+		TAB = FabricItemGroup.builder()
+			.icon(() -> new ItemStack(AtmBlock.INST))
+			.title(Component.literal("Fex's Small Money Mod"))
+			.displayItems((con, output) -> {
+				output.accept(AtmBlock.INST);
+				output.accept(MobileAtm.INST);
+				for(MoneyItem item : MoneyItem.sorted) output.accept(item);
+			})
+			.build();
+		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, ResourceLocation.parse("fsmm:main"), TAB);
 	}
 
 }
