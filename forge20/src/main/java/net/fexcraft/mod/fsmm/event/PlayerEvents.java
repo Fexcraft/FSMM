7556bfa611c17bc12a3b91ebef4a9ad732f7cdb5
Ferdinand Calo <fexcalo@gmail.com>
2026-01-08 20:41:13 +0100

    Update PlayerEvents.java

--- forge20/src/main/java/net/fexcraft/mod/fsmm/event/PlayerEvents.java
+++ forge20/src/main/java/net/fexcraft/mod/fsmm/event/PlayerEvents.java
@@ -5,6 +5,7 @@ import net.fexcraft.mod.fsmm.data.Account;
 import net.fexcraft.mod.fsmm.util.Config;
 import net.fexcraft.mod.fsmm.util.DataManager;
 import net.fexcraft.mod.fsmm.util.ItemManager;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -21,18 +22,18 @@ public class PlayerEvents {
     public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event){
     	if(event.getEntity().level().isClientSide) return;
 		FSMM.LOGGER.info("Loading account of " + event.getEntity().getName() + " || " + event.getEntity().getGameProfile().getId().toString());
-    	Account account = DataManager.getAccount("player:" + event.getEntity().getGameProfile().getId().toString(), false, true);
-    	if(Config.NOTIFY_BALANCE_ON_JOIN){
+    	Account account = DataManager.getAccount("player:" + event.getEntity().getGameProfile().getId().toString(), 2);
+    	account.addHolder(event.getEntity());
+		if(Config.NOTIFY_BALANCE_ON_JOIN){
 			event.getEntity().sendSystemMessage(getFormatted("&m&3Balance &r&7(in bank)&0: &a" + Config.getWorthAsString(account.getBalance())));
     		event.getEntity().sendSystemMessage(getFormatted("&m&3Balance &r&7(in Inv0)&0: &a" + Config.getWorthAsString(ItemManager.countInInventory(event.getEntity()))));
     	}
-    	if(account.lastAccessed() >= 0) account.setTemporary(false);
     }
     
     @SubscribeEvent
     public static void onPlayerLogout(PlayerEvent.PlayerLoggedOutEvent event){
-		FSMM.LOGGER.info("Unloading account of " + event.getEntity().getName() + " || " + event.getEntity().getGameProfile().getId().toString());
-		DataManager.unloadAccount("player", event.getEntity().getGameProfile().getId().toString());
+		FSMM.LOGGER.info("Unlinking account of " + event.getEntity().getName() + " || " + event.getEntity().getGameProfile().getId().toString());
+		DataManager.unholdPlayerAccount(event.getEntity().getGameProfile().getId(), Player.class);
     }
     
 }
